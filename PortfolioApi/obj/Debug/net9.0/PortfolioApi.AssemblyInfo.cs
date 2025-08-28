8ed117578d803973962ca6c5682cecec78f6cc69
Trevor Clawson <trevorbclawson@gmail.com>
2025-08-28 08:19:45 -0700

    Initial database migration

--- PortfolioApi/obj/Debug/net9.0/PortfolioApi.AssemblyInfo.cs
+++ PortfolioApi/obj/Debug/net9.0/PortfolioApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b944f4ae73f8bc18d4aa3b33f1220c2aa86f0ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+343ea5d587401bb43e809aa48391e7bdaec1e98e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
